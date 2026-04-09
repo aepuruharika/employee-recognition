1ae2f8901d26b01081716754dd30723ede8def5c
adepu.shreya <adepu.shreya@globallogic.com>
2026-04-09 10:30:50 +0530

    Added Gateway Application

--- rewardService/src/main/java/com/gl/rewardservice/client/RewardFeignClient.java
+++ rewardService/src/main/java/com/gl/rewardservice/client/RewardFeignClient.java
@@ -3,11 +3,9 @@ package com.gl.rewardservice.client;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "REWARD-SERVICE")
-public interface RecognitionFeignClient{
+public interface RewardFeignClient {
 
     @GetMapping("/employees/{id}")
     String getEmployeeById(@PathVariable("id") Long id);
